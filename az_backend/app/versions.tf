bff6ca33a74e28592d13c26c7dabaa4dedeb58ce
azdeivids <deivids.egle@gmail.com>
2023-04-06 16:57:42 +0100

    Configured backend

--- az_backend/app/versions.tf
+++ az_backend/app/versions.tf
@@ -11,9 +11,9 @@ terraform {
   }
   backend "azurerm" {
     resource_group_name  = "rg-terraform-uks-backend"
-    storage_account_name = "st6zyw77uj"
-    container_name       = "tfstate6zyw77uj"
-    key                  = "application.tfstate"
+    storage_account_name = "stwiiguikn"
+    container_name       = "tfstate-wiiguikn"
+    key                  = "backend_app.tfstate"
   }
 }
 
